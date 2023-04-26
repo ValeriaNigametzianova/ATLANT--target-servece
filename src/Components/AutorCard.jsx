9b33242b2826bf9b8581f2c07fb0c7358c864a93
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2023-04-26 16:35:29 +0300

    Update AutorCard.jsx

--- src/Components/AutorCard.jsx
+++ src/Components/AutorCard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Avatar from '../image/avatar_autor.png'
 import '../css/autorCard.css'
 
-const AutorCard = () => {
+const AutorCard = ({ name, info }) => {
   return (
     <div className="autorCard">
       <div className="avatar">
         <img className="avatar" src={Avatar} alt="" />
       </div>
       <div className="autor_info">
-        <div className="h4">Полина Кровякова</div>
-        <div className="h5">Тим-лид в ЛидерТаск Бизнес</div>
+        <div className="h4">{name}</div>
+        <div className="h5">{info}</div>
       </div>
     </div>
   )
